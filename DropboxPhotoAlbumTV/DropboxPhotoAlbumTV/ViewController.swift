8e8077eb020fdf64075d19f925b57d2331dc5aba
Garrett Richards <garrettrichards@gmail.com>
2016-01-04 00:38:24 -0800

    cleanup viewController, had a lot of cruft

--- DropboxPhotoAlbumTV/DropboxPhotoAlbumTV/ViewController.swift
+++ DropboxPhotoAlbumTV/DropboxPhotoAlbumTV/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import SwiftyDropbox
 
 class ViewController: UIViewController {
-
+    
+    private var images: [String]?
+    private var photoMediaManager:PhotoManager?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configurePhotoManager()
+        getUserInfo()
+        getImages()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //  MARK: - configure
+    
+    private func configurePhotoManager() {
+        if let client = Dropbox.authorizedClient {
+            photoMediaManager = PhotoManager(dropboxClient: client)
+        }
     }
 
+    private func getUserInfo() {
+        if let client = Dropbox.authorizedClient {
+            client.users.getCurrentAccount().response { response, error in
+                print("*** Get current account ***")
+                if let account = response {
+                    print("Hello \(account.name.givenName)!")
+                } else {
+                    print(error!)
+                }
+            }
+        }
+        else {
+            print("no dropbox client")
+        }
+    }
+    
+    private func getImages() {
+        photoMediaManager?.getPhotoURLsFromPath("/Camera Uploads") { [weak self] (images:[String]) -> Void  in
+            print("fetched \(images.count) image urls")
+            self?.images = images
+            self?.nextImage()
+        }
+    }
 
+    private func nextImage() {
+        let randomPhotoPath = images?.randomItem()
+        if let path = randomPhotoPath {
+            photoMediaManager?.getImage(path, handler: { [weak self] (image) -> Void in
+                self?.imageView.image = image
+            })
+        }
+    }
+
+    //  TODO - figure this all out (remote button presses)
+    override func pressesBegan(presses: Set<UIPress>, withEvent event: UIPressesEvent?) {
+        super.pressesBegan(presses, withEvent: event)
+        for press in presses {
+            switch (press.type) {
+            case .PlayPause:
+                nextImage()
+                break
+                //            case .UpArrow:
+                //            case .DownArrow:
+                //            case .LeftArrow:
+                //            case .RightArrow:
+                //            case .Select:
+                //            case .Menu:
+                //            case .PlayPause:
+                //                break
+            default:
+                break
+            }
+        }
+    }
 }
 
