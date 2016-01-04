d0f2799e3badcac7839963448c1e26fd8b076d53
Garrett Richards <garrettrichards@gmail.com>
2016-01-04 00:28:17 -0800

    remove extesion on viewController

--- DropboxPhotoAlbumTV/DropboxPhotoAlbumTV/UIViewController+Extensions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UIViewController+Extensions.swift
-//  DropboxPhotoAlbumTV
-//
-//  Created by Garrett Richards on 1/2/16.
-//  Copyright © 2016 Garrett Richards. All rights reserved.
-//
-
-import Foundation
